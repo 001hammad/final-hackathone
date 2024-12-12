8e68a4a9a2d51170be8e189097e5548f91e8c817
001hammad <167647584+001hammad@users.noreply.github.com>
2024-12-12 16:49:52 +0500

    update

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -31,13 +31,14 @@ const Navbar: React.FC = () => {
                 <div className="flex items-center space-x-4">
                     {/* Desktop navigation links */}
                     <Link href="/" className="hover:text-yellow-500 cursor-pointer text-white">Home</Link>
+                    <Link href="/Menupage" className="hover:text-yellow-500 text-white">Menu</Link>
                     <Link href="/blog" className="hover:text-yellow-500 text-white">Blog</Link>
                     <Link href="/about" className="hover:text-yellow-500 text-white">About</Link>
                     <Link href="/loginpage" className="hover:text-yellow-500 text-white">SignUp</Link>
-                    <Link href="/Menupage" className="hover:text-yellow-500 text-white">Menu</Link>
                     <Link href="/404Error" className="hover:text-yellow-500 text-white">Pages</Link>
+                    <Link href="/OurShop" className="hover:text-yellow-500 text-white">Shop</Link>
                     <Link href="/OurChefs" className="hover:text-yellow-500 text-white">Chefs</Link>
-                    <Link href="/contact" className="hover:text-yellow-500 text-white">Contact</Link>
+                    {/* <Link href="/contact" className="hover:text-yellow-500 text-white">Contact</Link> */}
                 </div>
 
                 {/* Search and Cart for Desktop */}
---
+++ src/app/OurShop/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+const OurShop = () => {
+  return (
+    <div className='bg-white'>
+        <Navbar/>
+        <Hero/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default OurShop
